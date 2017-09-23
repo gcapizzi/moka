e378995da18ebdd020042d75ffec50fa86c5f8d0
Giuseppe Capizzi <gcapizzi@pivotal.io>
2017-09-23 23:32:12 +0100

    Fail on unstubbed method invocation

--- double.go
+++ double.go
@@ -1,20 +1,36 @@
 package moka
 
+import "fmt"
+
 type Double interface {
 	StubMethod(methodName string, args []interface{}, returnValues []interface{})
 	Call(methodName string, args ...interface{}) []interface{}
 }
 
 type StrictDouble struct {
-	stubs map[string][]interface{}
+	stubs       map[string][]interface{}
+	failHandler FailHandler
 }
+
+type FailHandler func(message string)
 
 func NewStrictDouble() StrictDouble {
 	return StrictDouble{stubs: make(map[string][]interface{})}
 }
 
+func NewStrictDoubleWithFailHandler(failHandler FailHandler) StrictDouble {
+	return StrictDouble{stubs: make(map[string][]interface{}), failHandler: failHandler}
+}
+
 func (d StrictDouble) Call(methodName string, args ...interface{}) []interface{} {
-	return d.stubs[methodName]
+	returnValues, methodIsStubbed := d.stubs[methodName]
+
+	if !methodIsStubbed {
+		d.failHandler(fmt.Sprintf("No stub for method '%s'", methodName))
+		return nil
+	}
+
+	return returnValues
 }
 
 func (d StrictDouble) StubMethod(methodName string, args []interface{}, returnValues []interface{}) {
--- double_test.go
+++ double_test.go
@@ -7,20 +7,47 @@ import (
 	. "github.com/onsi/gomega"
 )
 
+var testFailHandlerInvoked = false
+var testFailMessage = ""
+
+func testFailHandler(message string) {
+	testFailHandlerInvoked = true
+	testFailMessage = message
+}
+
+func resetTestFail() {
+	testFailHandlerInvoked = false
+	testFailMessage = ""
+}
+
 var _ = Describe("StrictDouble", func() {
 	var double StrictDouble
 
 	BeforeEach(func() {
-		double = NewStrictDouble()
+		resetTestFail()
+		double = NewStrictDoubleWithFailHandler(testFailHandler)
 	})
 
-	Context("when a method is stubbed", func() {
+	Context("when a stubbed method is called", func() {
 		BeforeEach(func() {
 			double.StubMethod("UltimateQuestion", []interface{}{}, []interface{}{42, nil})
 		})
 
 		It("returns the stubbed return values", func() {
-			Expect(double.Call("UltimateQuestion")).To(Equal([]interface{}{42, nil}))
+			returnValues := double.Call("UltimateQuestion")
+
+			Expect(returnValues).To(Equal([]interface{}{42, nil}))
+			Expect(testFailHandlerInvoked).To(BeFalse())
+		})
+	})
+
+	Context("when an unknown method is called", func() {
+		It("fails the test", func() {
+			returnValues := double.Call("UnstubbedMethod")
+
+			Expect(returnValues).To(BeNil())
+			Expect(testFailHandlerInvoked).To(BeTrue())
+			Expect(testFailMessage).To(Equal("No stub for method 'UnstubbedMethod'"))
 		})
 	})
 })
